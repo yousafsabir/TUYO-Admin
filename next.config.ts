74e8e7209ee650925c7f30e838681e9b210ae071
yousafsabir <myousafdev@gmail.com>
2025-07-06 12:56:45 +0500

    chore: updated next config to not allow eslint & ts errors while building

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import { NextConfig } from 'next'
 import createNextIntlPlugin from 'next-intl/plugin'
 
 const nextConfig: NextConfig = {
-	eslint: {
-		ignoreDuringBuilds: true,
-	},
-	typescript: {
-		ignoreBuildErrors: true,
-	},
 	images: {
 		unoptimized: true,
 	},
